b27596a5adbf2ea4b0265603353d853a0dd6b9e0
Miguelangel <mflechaalderete@icloud.com>
2026-08-14 20:45:40 -0300

    Add product structure and input/output functionality

--- ejercicio01.cpp
+++ ejercicio01.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
 
+struct Producto{
+	
+	char nombre[30];
+	int codigo;
+	float precio;
+}producto1;
+
+int main (){
+	cout<<"\n Ingrese el nombre: ";
+	cin.getline(producto1.nombre,30,'\n');
+	cout<<"\n Ingrese el codigo: ";
+	cin>>producto1.codigo;
+	cout<<"\n Ingrese el precio: ";
+	cin>>producto1.precio;
+	cout<<"\n\n...DATOS DEL PRODUCTO..."<<endl;
+	cout<<"\n Nombre: "<<producto1.nombre<<endl;
+	cout<<"\n codigo: "<<producto1.codigo<<endl;
+	cout<<"\n precio: "<<producto1.precio<<endl;
+}
